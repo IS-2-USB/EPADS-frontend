5c9fe6fb43dae8fe07299a81b622957bb3dfdb66
Daniela <danielacaballer12@gmail.com>
2022-03-10 19:46:31 -0400

    cambio en  icono de search

--- src/searchBar.js
+++ src/searchBar.js
@@ -11,7 +11,7 @@ const SearchBar = () => {
                 label="Buscar"
             />
             <Button variant="outlined">
-                <SearchIcon />
+                <SearchIcon style={{color : "black"}}/>
             </Button>
         </form>
     );
