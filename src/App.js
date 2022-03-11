3c985ed2aad75b2d36fc2e6796bbe40d36b1a837
cristopherBB <38765189+cristopherBB@users.noreply.github.com>
2022-03-10 20:03:58 -0400

    update
    vistas login y registro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.scss";
 import Dashboard from "./views/Dashboard/Dashboard";
+import Login from "./views/login/Login.js";
+import SignUp from "./views/signUp/signUp.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
+  /*return (
+    <>
+      <SignUp />
+    </>
+    /*<>
+      <Login />
+    </>
     <>
       <Dashboard />
     </>
+  );*/
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="sign-up" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
