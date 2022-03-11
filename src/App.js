785b5c9f1a38aa7c9e63e02bdde5010f5796be78
Carlos Gonzalez <carlosegl10@hotmail.com>
2022-03-10 22:08:07 -0400

    add private routes and authContext

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import Dashboard from "./views/Dashboard/Dashboard";
 import Login from "./views/login/Login.js";
 import SignUp from "./views/signUp/signUp.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import PrivateRoute from "./auth/PrivateRoute";
+import React from "react";
+const theme = createTheme();
 
 function App() {
-  /*return (
-    <>
-      <SignUp />
-    </>
-    /*<>
-      <Login />
-    </>
-    <>
-      <Dashboard />
-    </>
-  );*/
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route exact path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Login />} />
+            <Route exact path="/sign-up" element={<SignUp />} />
+            <Route
+              exact
+              path="/dashboard"
+              element={<PrivateRoute redirectTo="/" Component={Dashboard} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
