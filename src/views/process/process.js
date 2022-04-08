3d440fd37cbd3a4a5599b375f86419f4f9f44f2e
danielazorrilla <danielazorrilla.dezg@gmail.com>
2022-04-08 15:44:54 -0400

    Historia 5
    Falta integrar con el back

--- src/views/process/process.js
+++ src/views/process/process.js
@@ -43,7 +43,6 @@ export default function Process() {
   const { state } = useAuth();
   const [currentPage, setCurrenPage] = useState(1);
   const [pageCount, setPageCount] = useState(0);
-  const queryClient = useQueryClient();
   const [editable, setEditable] = useState(null);
   const [description, setDescription] = useState("");
   const [type, setType] = useState("Ninguno");
@@ -53,33 +52,31 @@ export default function Process() {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isOpenModal3, setIsOpenModal3] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-  const [process, setProcess] = useState([]);
-  
-  // Ejemplo para cablear
-  //const [datos, setDatos] = useState([[{id: 1, proceso: "prueba", valor: 30}, {id: 2, proceso: "hola", valor: "No"}],
-  //                                    [{id: 1, proceso: "suministro", valor: "Si"}, {id: 2, proceso: "suministro2", valor: 60}]]);
+  const [projects, setProjects] = useState([]);
+
+
+  /* Process data */
+  const [datos, setDatos] = useState([[{id: 1, proceso: "Acuerdo de Contrato", valor: 100}, {id: 2, proceso: "Monitoreo de Proveedores", valor: 80}, {id: 3, proceso: "Requisitos Técnicos", valor: 100}],
+                                      [{id: 1, proceso: "Requisitos Legales", valor: 100}, {id: 2, proceso: "Requisitos de Proveedores", valor: 60}],
+                                      [{id: 1, proceso: "Solicitud de Propuestas", valor: 100}, {id: 2, proceso: "Calificación del Proveedor", valor: 70}] ]);
+
+  /** Ejemplo para cablear
+  const [datos, setDatos] = useState([[{id: 1, proceso: "prueba", valor: 30}, {id: 2, proceso: "hola", valor: "No"}],
+                                      [{id: 1, proceso: "suministro", valor: "Si"}, {id: 2, proceso: "suministro2", valor: 60}]]);
+    */
+
   const router = useNavigate();
   const { dispatch } = useAuth();
-
-  /** GET PROCESSES */
-
-  //const { data } = useQuery("projects", () =>
-  //  fetchService({ url: `/projects/getall/${state.id || 1}` })
-  //);
-
-  const { data } = useQuery("process", () => 
-    fetchService({ url: `/process/all` })
+  const { data } = useQuery("projects", () =>
+    fetchService({ url: `/projects/getall/${state.id || 1}` })
   );
 
-
-
-  /** GET PROCESSES */
   useEffect(() => {
     if (data) {
       let filterData = data;
       if (searchValue) {
-        filterData = data.filter((process) =>
-          process.name
+        filterData = data.filter((project) =>
+          project.description
             .toLowerCase()
             .includes(searchValue.toLocaleLowerCase())
         );
@@ -87,46 +84,22 @@ export default function Process() {
       const startData = (currentPage - 1) * 5;
       const endData = startData + 5;
       setPageCount(Math.ceil(filterData.length / 5));
-      setProcess(filterData.reverse().slice(startData, endData));
+      setProjects(filterData.reverse().slice(startData, endData));
     }
   }, [data, currentPage, searchValue]);
-
-
-  /**
-  const { mutate: deleteProjectMutation } = useMutation(
-    (id) => {
-      // se debe hacer un fetch para eliminarlo en la base de datos
-
-      
-      for (let index = 0; index < datos[group].length; index++) {
-        const element = datos[group][index];
-        if(element.id === id){
-          return datos[group].splice(index, 1);
-        }
-      }
-    },
-  );
-  */
 
   /** Delete process */
   const { mutate: deleteProcessMutation } = useMutation(
     (id) => {
-      return fetchService({
-        url: `/process/delete/${id}`,
-        method: "DELETE",
-        token: "",
-      });
+      // se debe hacer un fetch para eliminarlo en la base de datos
+      for (let index = 0; index < datos[category].length; index++) {
+        const element = datos[category][index];
+        if(element.id === id){
+          return datos[category].splice(index, 1);
+        }
+      }
     },
-
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries("process");
-      },
-      onError: (error) => console.log(error),
-    }
   );
-
-
 
   function user() {
     router("/users");
@@ -140,7 +113,7 @@ export default function Process() {
     router("/logger")
   }
 
-  function routeProcess(){
+  function process(){
     router("/process")
   }
 
@@ -167,26 +140,33 @@ export default function Process() {
     setIsOpenModal3(false);
   };
 
-  function update(updateValor, id, field){
+  /** Create process */
+  function createProcess() {
+      let index = datos[category].length + 1;
 
-    /*
-    for (let index = 0; index < datos[group].length; index++) {
-      const element = datos[group][index];
+      datos[category].splice(index, 0, {id: index, proceso: description, valor: 100})
+      setDescription("")
+  }
+
+  /** Update process */
+  function update(updateValor, id, field){
+    for (let index = 0; index < datos[category].length; index++) {
+      const element = datos[category][index];
       if(element.id === id){
         if(field === "proceso"){
-          return datos[group][index].proceso = updateValor;
+          return datos[category][index].proceso = updateValor;
         }
         else{
-          return datos[group][index].valor = updateValor;
+          return datos[category][index].valor = updateValor;
         }
         
       }
     }
-    */
   }
 
   const editProject = (id, currentDesc) => {
     update(description, id, "proceso");
+    setDescription("")
   };
 
   const editValor = (id) => {
@@ -199,54 +179,32 @@ export default function Process() {
         update(type, id, "valor")
       }
       else{
-        alert("el valor debe estar entre 0 y 100")
+        alert("El valor debe estar entre 0 y 100")
       }
     }
     else{
-      alert("los tipos deben de coincidir")
+      alert("Los tipos deben de coincidir")
     }
   };
-
-  /** ADD PROCESS */
-  const { mutate } = useMutation(
-    (values) => {
-      return fetchService({
-        url: "/process/add",
-        params: {
-          name: values.description,
-          group: values.category,
-          qualitative_value: "QualitativeEnum.unachieved",
-          quantitative_value: 10,
-        },
-        method: "POST",
-        token: "",
-      });
-    },
-    {
-      onSuccess: () => {
-        closeModal();
-        queryClient.invalidateQueries("process");
-      },
-      onError: (error) => console.log(error),
-    }
-  );
-
-  const createProcess = () => {
-    mutate({ description, category });
-  };
-
-  /////////////////////////////////////////
-
 
 
   const onChangeType = ({ target }) => {
     setType(target?.value);
   };
 
+  const [category, setCategory] = React.useState(0);
+  //const [group, setGroup] = React.useState(0);
+  const [valorAddGroup, setValorAddGroup] = React.useState("");
 
-  const [category, setCategory] = React.useState('');
-  const [group, setGroup] = React.useState(0);
-  const [valorAddGroup, setValorAddGroup] = React.useState("")
+  const [group, setGroup] = React.useState(["Adquisición", "Suministro"])
+  const [currentGroup, setCurrentGroup] = React.useState(group[0])
+
+  /* Add new group to list */
+  function createGroup() {
+    let index = group.length + 1
+    group.splice(index, 0, description)
+    setDescription("")
+  }
 
   const handleChange = (event) => {
     setCategory(event.target.value);
@@ -257,21 +215,31 @@ export default function Process() {
   };
 
   const handleChangeGroup = (event) => {
-    setGroup(event.target.value);
+    //setGroup(event.target.value);
+    setCurrentGroup(event.target.value);
   };
+
+  /** Get category name */
+  const getCategory = () => {
+      if (category==0) return "Procesos Primarios"
+      if (category==1) return "Procesos Organizacionales"
+      if (category==2) return "Procesos de Soporte"
+  }
 
   return (
     <>
       <div className={styles.container}>
         <h1>Procesos</h1>
-
         <Dialog open={isOpenModal3} onClose={closeModal3}>
           <DialogTitle>Crear nuevo grupo de procesos</DialogTitle>
           <div className={styles.form}>
             <TextField
               label="Nombre del grupo"
               onChange={handleOnChangeDescription}
+              value={description}
             />
+
+          {/* 
           <FormControl className={styles.selectModal} style={{ marginTop: "20px"}}>
             <InputLabel id="demo-simple-select-label">Valor</InputLabel>
             <Select
@@ -281,17 +249,23 @@ export default function Process() {
                 label="valorAddGroup"
                 onChange={handleChangeValor}
             >
-                {/* colocar los procesos que vengan de la base de datos */}
+                {/* colocar los procesos que vengan de la base de datos /}
                 <MenuItem value={"cualitativo"}>Cualitativo</MenuItem>
                 <MenuItem value={"cuantitativo"}>Cuantitativo</MenuItem>
             </Select>
           </FormControl>
+          */}
+
+
           </div>
           <div className={styles.controls}>
             <Button variant="outlined" onClick={closeModal3}>
               Cancelar
             </Button>
-            <Button variant="contained" onClick={closeModal3}>
+            <Button variant="contained" onClick={() => { 
+              createGroup();
+              closeModal3();
+            }}>
               Crear
             </Button>
           </div>
@@ -302,13 +276,17 @@ export default function Process() {
             <TextField
               label="Nombre del proceso"
               onChange={handleOnChangeDescription}
+              value={description}
             />
           </div>
           <div className={styles.controls}>
             <Button variant="outlined" onClick={closeModal}>
               Cancelar
             </Button>
-            <Button variant="contained" onClick={createProcess}>
+            <Button variant="contained" onClick={() => { 
+              createProcess();
+              closeModal();
+            }}>
               Crear
             </Button>
           </div>
@@ -347,9 +325,9 @@ export default function Process() {
                     onChange={handleChange}
                 >
                     {/* colocar los procesos que vengan de la base de datos */}
-                    <MenuItem value={"Primarios"}>Procesos primarios</MenuItem>
-                    <MenuItem value={"Organizacionales"}>Procesos organizacionales</MenuItem>
-                    <MenuItem value={"Soporte"}>Procesos de soporte</MenuItem>
+                    <MenuItem value={0}>Procesos primarios</MenuItem>
+                    <MenuItem value={1}>Procesos organizacionales</MenuItem>
+                    <MenuItem value={2}>Procesos de soporte</MenuItem>
                 </Select>
             </FormControl>
             <Button variant="text" color="success" onClick={() => { setIsOpenModal(true);}}>
@@ -357,11 +335,13 @@ export default function Process() {
             </Button>
         </div>
         <div>
-            <h5>Categoria: {category}</h5>
+            <h5>Categoría: {getCategory()}</h5>
             <div className={styles.group}>
                 <h5>Grupo de procesos: </h5>
                 <FormControl className={styles.form}>
                     <InputLabel id="demo-simple-select-label">Grupo</InputLabel>
+
+                    {/*
                     <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
@@ -369,10 +349,20 @@ export default function Process() {
                         label="group"
                         onChange={handleChangeGroup}
                     >
-                      {/* colocar los grupos que vengan de la base de datos */}
-                        <MenuItem value={0}>Adquisicion</MenuItem>
+                      {/* colocar los grupos que vengan de la base de datos /}
+                        <MenuItem value={0}>Adquisición</MenuItem>
                         <MenuItem value={1}>Suministro</MenuItem>
                     </Select>
+                    */}
+
+                    <Select value={currentGroup} onChange={handleChangeGroup}>
+                        {group?.map(element => {
+                            return (
+                                <MenuItem key={element} value={element}>{element}</MenuItem>
+                            )
+                        })}
+                    </Select>
+
                 </FormControl>
                 <Button variant="text" color="success" onClick={() => { setIsOpenModal3(true);}}>
                   <AddBoxIcon />
@@ -391,7 +381,7 @@ export default function Process() {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {process.map((row) => ( 
+                {datos[category].map((row) => (
                   <TableRow
                     key={row.id}
                     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -405,24 +395,24 @@ export default function Process() {
                           label=""
                           variant="standard"
                           onChange={handleOnChangeDescription}
-                          defaultValue={row.name}
+                          defaultValue={row.proceso}
                           onKeyDown={(e) => {
                             e.key === "Enter" && editProject(row.id);
                           }}
                         />
                       ) : (
-                        row.name
+                        row.proceso
                       )}
                     </TableCell>
                     <TableCell align="center" style={{width: "11rem"}}>
                       { (
-                        row.quantitative_value + "%"
+                        row.valor + "%"
                       )}
                       <div
                           onClick={() => {
-                            setOldType(row.quantitative_value)
+                            setOldType(row.valor)
                             setIsOpenModal2(true);
-                            setType(row.quantitative_value)
+                            setType(row.valor)
                             setCurrentId(row.id)
                           }}
                           style={{ cursor: "pointer", float: "right" }}
@@ -435,8 +425,8 @@ export default function Process() {
                         <div
                           onClick={() => {
                             setEditable(row.id)
-                            setDescription(row.name)
-                            setType(row.quantitative_value)
+                            setDescription(row.proceso)
+                            setType(row.valor)
                           }}
                           style={{ cursor: "pointer" }}
                         >
@@ -457,7 +447,7 @@ export default function Process() {
                           onClick={() => setEditable(false)}
                         >
                           <div
-                            onClick={() => editProject(row.id, row.name)}
+                            onClick={() => editProject(row.id, row.proceso)}
                           >
                             <SaveIcon style={{ color: "green" }} />
                           </div>
@@ -501,7 +491,7 @@ export default function Process() {
             { name: "Proyectos", redirect: () => {dash()}, icon: <FolderIcon /> },
             { name: "Usuarios", redirect: () => {user()}, icon: <PeopleIcon /> },
             { name: "Eventos", redirect: () => {logger()}, icon: <EventNoteIcon /> },
-            { name: "Procesos", redirect: () => {routeProcess()}, icon: <FolderIcon /> },
+            { name: "Procesos", redirect: () => {process()}, icon: <FolderIcon /> },
           ].map(({ name, redirect, icon }, index) => (
             <ListItem button onClick={redirect} key={name}>
               <ListItemIcon>{icon}</ListItemIcon>
